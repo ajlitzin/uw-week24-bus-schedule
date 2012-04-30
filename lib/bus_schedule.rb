4f357b561168813a0a08d51c49c7ee8d10d4ec97
Andy Litzinger <andy.litzinger@gmail.com>
2012-04-29 22:11:40 -0700

    refactored code- but getting error

--- lib/bus_schedule.rb
+++ lib/bus_schedule.rb
@@ -1,9 +1,14 @@
 class Stop
-  attr_accessor :stop_number, :intersection, :departure_time, :routes_serving_stop, :notes
+  attr_reader :stop_number, :intersection, :departure_time, :routes_serving_stop, :notes
   
-  def shelter=(shelter)
-    @shelter = shelter
+  def initialize(stop_number,departure_time, routes_serving_stop, shelter = false, notes = [])
+    @stop_number = stop_number
+	@departure_time = departure_time
+	@routes_serving_stop = routes_serving_stop
+	@notes = notes
+	@shelter = shelter
   end
+  
   def shelter?
     if shelter.is_a(TrueClass)
 	  @shelter = shelter
@@ -15,53 +20,42 @@ class Stop
   end
 end
 
+class BusRoutes
+require 'pp'
+# BusRoutes collects mutiple Route objectes
+# a Route obj collects Stop objects
+  def self.route(name,&block)
+    @routes ||= {}
+	route = Route.new(name)
+	route.instance_eval(&block)
+	pp route
+	@routes << route
+	route
+	
+  end
+end
+
 class Route
 require 'pp'
-
+attr_reader :name
   def initialize(route_name)
     @route_name = route_name
   end
   attr_reader :stops
 
-  def stop(intersection, time, stop_number, routes_serving_stop = [], shelter = false, other = {})
-    # create and define a stop object
-	# a Route obj collects stop objects
-	# some larger collection collects mutiple Route objectes
-	# e.g. Route.new
-	# Route[<route_number>].<stop_number>.property
-	# this wont really work unless i monkey patch Fixnum
-	# how else can 
-	this_stop = Stop.new
-	this_stop.stop_number = stop_number
-	this_stop.intersection = intersection
-	this_stop.departure_time = time
-	this_stop.routes_serving_stop = routes_serving_stop
-	this_stop.shelter = shelter
-	pp this_stop
-	#so now i have a stop object, but it disappears as soon as the next
-	# stop def comes through the block loop
-	# how do i persist it?
+  def stop(intersection, departure_time, stop_number, routes_serving_stop = [], shelter = false, other = {})	
+	@stops ||= {}
+	#(stop_name,departure_time, routes_serving_stop, shelter = false, notes = [])
+	@stops[intersection] = Stop.new stop_number, departure_time, routes_serving_stop, shelter, other
+	#pp @stops 
+  end
     
-  end
-  
-  
-  def self.schedule(route_name,&schedule_block)
-     route = self.new
-	 route.instance_eval(&schedule_block) if schedule_block
-	 #puts route
-	 #pp route
-	 route
-	 
-  end
-  
-  
-  
 end
 
 
-Route.schedule "4" do
+fifteen_schedule = BusRoutes.route "15" do
   # origin
-  stop "50th Ave & Funky St", "12:00", 1, %w[5,51], false, "coolville"
+  stop "50th Ave & Funky St", "12:00", 1, %w[5,51], true, "coolville"
   stop "40th Ave & Funky St", "12:10", 2
   stop "30th Ave & Funky St", "12:20", 3
   stop "20th Ave & Funky St", "12:30", 4
@@ -69,3 +63,4 @@ Route.schedule "4" do
   #terminus
   stop "1st Ave & Funky St", "12:50", 6, "funkytown"
 end
+pp fifteen_schedule
